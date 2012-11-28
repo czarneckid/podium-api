3d8e494acb975fb05f24180f54ab91d417d97b0b
David Czarnecki <me@davidczarnecki.com>
2012-11-28 14:39:07 -0500

    Adding production Redis instance

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,10 @@
-require 'redis'
+  require 'redis'
 require 'leaderboard'
 
 case ENV['RACK_ENV']
 when 'production'
   # Replace this with an appropriate Redis instance for production
+  $redis = Redis.new(:host => clingfish.redistogo.com, :port => 9677, :password => 'aa35b02b546723e18be6e59016093e88')
 else
   $redis = Redis.new
 end
